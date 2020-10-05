72e90062cba3c221c0db6f8c368bbc4801e72916
NaifAlDhaban <naif@aldhabaan.bz>
2020-10-05 20:42:43 +0300

    first commit

---
+++ wp-content/plugins/naif-plugin/naif-plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:     Naif First Plugin
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     naif-plugin
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Naif_Plugin
+ */
+
+// Your code starts here.
+add_action( 'admin_notices', function () {
+    echo 'Hello world!';
+    return;
+} );
